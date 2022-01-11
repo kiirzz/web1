f8586123bc7854ff181eb0e088af1792a7387928
kiirzz <hmtb2001@gmail.com>
2022-01-11 19:48:54 +0300

    assets

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// var clickmu = document.getElementById("mu");
+// var openmu = document.getElementsByClassName("modal");
+// clickmu.addEventListener("click", function() {openmuFunction()});
+
+// function openmuFunction() {
+//     openmu.style.display = 'flex';
+// }
+
+// var closebtn = document.getElementById("close");
+// closebtn.addEventListener("click", function() {closeFunction()});
+
+// function closeFunction() {
+//     openmu.style.display = 'none';
+// }
